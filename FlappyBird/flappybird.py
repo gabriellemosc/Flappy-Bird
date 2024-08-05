7f5861281e0a183baa98a9be6364c7b36d31c016
gabriellemosc <gabriel.lemos1910@gmail.com>
2024-08-05 19:33:55 -0300

    Adicionando Class Passaro

--- FlappyBird/flappybird.py
+++ FlappyBird/flappybird.py
@@ -17,3 +17,34 @@ pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
 pygame.font.init()
 
 FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+#Classes
+
+class Passaro():
+  IMGS = IMAGENS_PASSARO
+  #animacoes da rotacao
+  ROTACAO_MAXIMA = 25
+  VELOCIDADE_ROTACAO = 20
+  TEMPO_ANIMACAO = 5
+
+
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.angulo = 0
+    self.velocidade = 0
+    self.altura = self.y
+    self.tempo = 0
+    self.contagem_imagem = 0
+    self.imagem = IMGS[0]
+
+
+
+
+class Cano():
+  pass
+
+
+
+class Chao():
+  pass
