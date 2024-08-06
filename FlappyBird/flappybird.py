74d690d289b9533c19d7bb9fdc6bbb2040e0c29c
gabriellemosc <gabriel.lemos1910@gmail.com>
2024-08-06 11:35:11 -0300

    Criando Função Principal, interação do Usuário com o jogo

--- FlappyBird/flappybird.py
+++ FlappyBird/flappybird.py
@@ -184,3 +184,33 @@ def desenhar_tela(tela, passaros, canos,chao, pontos):
    tela.blit(texto, (TELA_ALTURA -10 - texto.get_width(), 10))
    chao.desenhar(tela)
    pygame.display.update()
+
+
+
+def main():
+   passaros = [Passaro(230, 350)]
+   chao = Chao(730)
+   canos = [Cano(700)]
+   tela = pygame.display.set_mode(TELA_LARGURA, TELA_ALTURA)
+   pontos = 0
+   relogio = pygame.time.Clock()
+
+   rodando = True
+
+   while rodando:
+      relogio.tick(30)
+
+      for evento in pygame.event.get():
+         if evento.type == pygame.QUIT:
+            rodando = False
+            pygame.quit()
+            quit()
+         if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE:
+               for passaro in passaros:
+                  passaro.pular()
+
+
+
+
+      desenhar_tela(tela, passaros, canos, chao, pontos)
