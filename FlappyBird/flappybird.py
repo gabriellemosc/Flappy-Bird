935d6687cd55a62574f29c75cdeb58a751e3b4bd
gabriellemosc <gabriel.lemos1910@gmail.com>
2024-08-06 08:46:18 -0300

    Criando cano

--- FlappyBird/flappybird.py
+++ FlappyBird/flappybird.py
@@ -20,7 +20,7 @@ FONTE_PONTOS = pygame.font.SysFont('arial', 50)
 
 #Classes
 
-class Passaro():
+class Passaro:
   IMGS = IMAGENS_PASSARO
   #animacoes da rotacao
   ROTACAO_MAXIMA = 25
@@ -100,9 +100,25 @@ def get_mask(self):
 
 
 
-class Cano():
-  pass
+class Cano:
+  DISTANCIA = 200
+  VELOCIDADE = 5
 
+
+  def __init__(self, x):
+     self.x = x
+     self.altura =  0
+     self.pos_topo = 0
+     self.pos_base = 0
+     self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO, False, True)
+     self.CANO_BASE = IMAGEM_CANO
+     self.passou = False
+     self.definir_altura()
+
+  def definir_altura(self):
+     self.altura = random.randrange(50, 450)
+     self.pos_base = self.altura - self.CANO_TOPO.get_height()
+     self.pos_base = self.altura + self.DISTANCIA
 
 
 class Chao():
